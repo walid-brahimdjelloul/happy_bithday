b188207c9b4fa7f2d9fc71f1e3156390f995e9a5
walid brahimdjelloul <103107369+walid-brahimdjelloul@users.noreply.github.com>
2023-03-07 21:53:36 +0100

    Add files via upload

---
+++ happy_bithday.py
@@ -0,0 +1,20 @@
+### Happy bithday to your mother
+HAPPY = """
+H   H   A   PPP   PPP   Y   Y
+H   H  A A  P   P P   P  Y Y
+H   H A   A P   P P   P   Y
+HHHHH AAAAA PPPP  PPPP    Y
+H   H A   A P     P       Y
+H   H A   A P     P       Y
+H   H A   A P     P       Y
+"""
+BITHDAY = """
+BBBB  IIIII TTTTT H   H DDDD    A   Y   Y
+B   B   I     T   H   H D   D  A A   Y Y
+B   B   I     T   H   H D   D A   A   Y
+BBBB    I     T   HHHHH D   D AAAAA   Y
+B   B   I     T   H   H D   D A   A   Y
+B   B   I     T   H   H D   D A   A   Y
+BBBB  IIIII   T   H   H DDDD  A   A   Y
+"""
+print('',HAPPY, BITHDAY)
